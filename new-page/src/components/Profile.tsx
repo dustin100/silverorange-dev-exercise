c9af39de0e9ebb01e95855c60e81fd4abbf3525f
Dustin Kelly <dmkelly100@gmail.com>
2026-03-19 22:41:33 -0400

    BUILD: Profile comp and styling

--- new-page/src/components/Profile.tsx
+++ new-page/src/components/Profile.tsx
@@ -1,8 +1,20 @@
+import styles from "./Profile.module.css";
+import profilePic from "../assets/profile-pic.png";
 
 
 const Profile = () => {
   return (
-    <div>to do</div>
+    <div className={styles.profile}>
+      <img
+        className={styles.avatar}
+        src={profilePic}
+        alt="John Smitherington"
+      />
+      <div>
+        <p className={styles.name}>John Smitherington</p>
+        <p className={styles.location}>Boise, Idaho</p>
+      </div>
+    </div>
   );
 };
 
